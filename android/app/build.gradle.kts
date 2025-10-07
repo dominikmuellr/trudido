2ad596f82a0f749fe7afb00a3074ad01eafb8b93
dominikmuellr <mllrdominik@gmail.com>
2025-10-07 16:15:56 +0200

    Add F-Droid metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,21 +21,25 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-    applicationId = "com.trudido.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.trudido.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("D:/keystores/trudido-release-key.jks")
+            storePassword = "^%o7^7#rK#r2J@3&59*H$798E"
+            keyAlias = "trudido-key"
+            keyPassword = "^%o7^7#rK#r2J@3&59*H$798E"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
