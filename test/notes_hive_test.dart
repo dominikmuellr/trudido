8f637c871f4ceef4c78cf6de4dd6ff32ef9af1a8
dominikmuellr <mllrdominik@gmail.com>
2025-11-21 11:25:44 +0100

    cleanup

--- test/notes_hive_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('Notes Feature Tests', () {
-    test('placeholder test', () {
-      expect(true, isTrue);
-    });
-  });
-}
--- test/utils/todo_txt_converter_test.dart
+++
@@ -1,1 +0,0 @@
-// All tests related to todo.txt have been removed as it is no longer used.
