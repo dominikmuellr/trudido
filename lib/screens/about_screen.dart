dcd899e51aa976a6f203f3a59b2ec88a0691fb7f
dominikmuellr <dmuellr.dev@gmail.com>
2026-02-11 10:37:11 +0100

    bump version number to 1.3.0 and buildnumber to 21

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -88,7 +88,7 @@ class _AboutScreenState extends State<AboutScreen> {
           ListTile(
             leading: Icon(Icons.tag, color: cs.primary),
             title: const Text('Version'),
-            subtitle: const Text('v1.2.7'),
+            subtitle: const Text('v1.3.0'),
           ),
           ListTile(
             leading: Icon(Icons.code, color: cs.primary),
@@ -127,7 +127,7 @@ class _AboutScreenState extends State<AboutScreen> {
             onTap: () => showLicensePage(
               context: context,
               applicationName: 'Trudido',
-              applicationVersion: 'v1.2.7',
+              applicationVersion: 'v1.3.0',
             ),
           ),
 
